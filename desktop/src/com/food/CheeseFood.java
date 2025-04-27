3d45be695fb375d5cc71fc48f2c080011245336a
Broken engineering <3267846129@qq.com>
2025-04-27 09:31:04 +0800

    修改刚体

--- desktop/src/com/food/CheeseFood.java
+++ desktop/src/com/food/CheeseFood.java
@@ -1,16 +1,29 @@
 package com.food;
 
 import com.Content;
+import com.kw.gdx.d3.actor.BaseActor3D;
 import com.kw.gdx.d3.actor.BaseActor3DGroup;
 import com.kw.gdx.d3.actor.ModelActor3D;
 import com.kw.gdx.d3.asset.Asset3D;
 
 public class CheeseFood extends FoodGroup {
+    private BaseActor3D noCutCheese;
+    private BaseActor3D cutCheese;
     public CheeseFood(){
         id = Content.CHEESE;
-        ModelActor3D food = new ModelActor3D(Asset3D.getAsset3D()
+        noCutCheese = new ModelActor3D(Asset3D.getAsset3D()
                 .getModel("kitchen/model/Cheese block.g3db"));
-        addActor3D(food);
+        addActor3D(noCutCheese);
         setScale(2,2,2);
+        cutCheese = new ModelActor3D(Asset3D.getAsset3D().getModel("kitchen/model/Cheese slice.g3db"));
+        addActor3D(cutCheese);
+        cutCheese.setVisible(false);
+    }
+
+    @Override
+    public void changeCutStatus() {
+        super.changeCutStatus();
+        cutCheese.setVisible(true);
+        noCutCheese.setVisible(false);
     }
 }
--- desktop/src/com/food/TomatoFood.java
+++ desktop/src/com/food/TomatoFood.java
@@ -6,11 +6,23 @@ import com.kw.gdx.d3.actor.ModelActor3D;
 import com.kw.gdx.d3.asset.Asset3D;
 
 public class TomatoFood extends FoodGroup {
+    private ModelActor3D noCutTomatoed;
+    private ModelActor3D cutTomatoed;
     public TomatoFood() {
         id = Content.TOMATO;
-        ModelActor3D food = new ModelActor3D(Asset3D.getAsset3D()
+        noCutTomatoed = new ModelActor3D(Asset3D.getAsset3D()
                 .getModel("kitchen/model/Tomato.g3db"));
-        addActor3D(food);
+        addActor3D(noCutTomatoed);
         setScale(2,2,2);
+        cutTomatoed = new ModelActor3D(Asset3D.getAsset3D().getModel("kitchen/model/Tomato slice.g3db"));
+        addActor3D(cutTomatoed);
+        cutTomatoed.setVisible(false);
+    }
+
+    @Override
+    public void changeCutStatus() {
+        super.changeCutStatus();
+        cutTomatoed.setVisible(true);
+        noCutTomatoed.setVisible(false);
     }
 }
